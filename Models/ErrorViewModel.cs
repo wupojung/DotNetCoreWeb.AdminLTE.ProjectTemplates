63ff6d34a2dabf294d5e3b906aafa346f0fff3e5
Po-Lung Wu <wupojung@gmail.com>
2024-02-08 16:12:03 +0800

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNetCoreWeb.AdminLTE.ProjectTemplates.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
